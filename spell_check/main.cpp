99392256086accaf6baeea730f8ddf58c80b6fa4
Kapatsa <31724594+Kapatsa@users.noreply.github.com>
2019-12-28 00:51:40 +0300

    Initial Commit

---
+++ spell_check/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  spell_check
+//
+//  Created by David Kapatsa on 28.12.2019.
+//  Copyright © 2019 David Kapatsa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
